b795683bb4ca9d7ae2b2c7c5ac04d53006051c59
Ievgen Iezhachenko <ievgen.iezhachenko@gmail.com>
2013-12-04 20:30:45 +0200

    Day 1, 2, 2.5

---
+++ cookbooks/webserver/attributes/default.rb
@@ -0,0 +1,3 @@
+override['apache']['default_site_enabled'] = false
+default['apache']['log_level'] = 'debug'
+default['apache']['aliases'] = ['url1.com','url2.com']
---
+++ cookbooks/webserver/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "apache2"
+include_recipe "apache2::mod_rewrite"
+
+template "#{node['apache']['dir']}/sites-available/cvepatch" do
+  owner 'root'
+  group node['apache']['root_group']
+  mode  '0644'
+  notifies :restart, 'service[apache2]'
+end
+
+apache_site 'cvepatch' do
+  enable true
+end
+
+cookbook_file "#{node['apache']['dir']}/htpasswd" do
+  owner 'root'
+  group node['apache']['root-group']
+  mode '0644'
+  backup false
+  #content "admin:$apr1$ejZO6aAi$9zUZFyNxkX7pHOfqnjs8/0"
+end
